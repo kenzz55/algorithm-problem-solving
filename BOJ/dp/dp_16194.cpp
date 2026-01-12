99cfb4728cdf9de61ae518cc25eaf6ca0ba3c2d5
kenzz55 <kenzz5tt@gmail.com>
2026-01-12 22:04:47 +0900

    ,

---
+++ BOJ/dp/dp_16194.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+
+    vector<int> P(N+1, 0);
+    vector<int> dp(N+1, 0);
+    for(int i = 1; i <= N; i++){
+        cin >> P[i];
+        dp[i] = P[i];
+    }
+
+
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= i; j++) {
+            dp[i] = min(dp[i], dp[i-j] + P[j]);
+        }
+    }
+
+    cout << dp[N];
+
+}
